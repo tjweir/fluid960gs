b785ebe49430d9df5ea3af867f5badcd84b65994
Liu Kang-min <gugod@gugod.org>
2009-01-31 08:18:31 +0800

    rewrote accordion.js with jQuery.

--- js/accordion.js
+++ js/accordion.js
@@ -1,14 +1,18 @@
-window.addEvent('domready', function() {
-	var accordion = new Accordion('h3.atStart', 'div.atStart', {
-		opacity: false,
-		onActive: function(toggler, element){
-			toggler.setStyle('font-weight', 'bold');
-			toggler.setStyle('background', '#fff');
-		},
-	 
-		onBackground: function(toggler, element){
-			toggler.setStyle('font-weight', 'normal');
-			toggler.setStyle('background', '#eee');
-		}
-	}, $('accordion'));
+jQuery(function($) {
+    $("#accordion").accordion({
+        'header': "h3.atStart"
+    }).bind("accordionchangestart", function(e, data) {
+        data.newHeader.css({
+            "font-weight": "bold",
+            "background": "#fff"
+        });
+
+        data.oldHeader.css({
+            "font-weight": "normal",
+            "background": "#eee"
+        });
+    }).find("h3.atStart:first").css({
+        "font-weight": "bold",
+        "background": "#fff"
+    });
 });
