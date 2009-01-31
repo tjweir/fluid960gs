9e576fa283da41bec34697ed747a997d401b1a1e
Liu Kang-min <gugod@gugod.org>
2009-01-31 09:06:13 +0800

    fix previous implementation.
    Also need to add a little animation stopper to prevent fx going crazy when mouse curosr
    goes to .kwick border.

--- js/kwick.js
+++ js/kwick.js
@@ -1,10 +1,17 @@
 jQuery(function ($) {
-    $("#kwick")
+    var animating = false;
+    $("#kwick .kwick")
         .bind("mouseenter", function(e) {
-            $(this).not(e.target).animate({ "width": 125 }, 200);
-            $(e.target).animate({ "width": 485 }, 200);
-        })
-        .bind("mouseleave", function(e) {
-            $(".kwick", this).animate({ "width": 215 }, 200);
+            if (animating) return false;
+
+            animating == true;
+            $("#kwick .kwick").not(this).animate({ "width": 125 }, 200);
+            $(this).animate({ "width": 485 }, 200, function() {
+                animating = false;
+            });
         });
+
+    $("#kwick").bind("mouseleave", function(e) {
+        $(".kwick", this).animate({ "width": 215 }, 200);
+    });
 });
